70d514bc2cca088f919adc973d285fe8257cfd20
Vusaldi <93197397+Vusaldi@users.noreply.github.com>
2022-12-20 18:18:33 +0400

    Update bot.py

--- mesajlar/bot.py
+++ mesajlar/bot.py
@@ -18,5 +18,5 @@ start = (
 )
 
 info = (
-"☑ Mən XAOS Federasiyasının Rəsmi Söhbət Botuyam\n\nℹ Botumuz Haqda Qısa Məlumat\n⚡ 𝕏𝔸𝕆𝕊 ℂℍ𝔸𝕋 𝔹𝕆𝕋  Sıradan Söhbət Və Avto Cavablayıcı Botudur. Bu Botun Əsas Özəlliyi Qruplarda Mesaj Sayın Çoxaltmaq , Vaxdınızı Əyləncəli Keçirmək Dir. 𝕏𝔸𝕆𝕊 ℂℍ𝔸𝕋 𝔹𝕆𝕋 u qruplarınlza Alaraq Vaxdınızı Əyləncəli Və Səmərəli Keçirə Bilərsiz.\n\n➕[❰ QRUPA ƏLAVƏ ET ❱➕](https://t.me/XAOS_Chatbot?startgroup=true)\n\n\n\**👨‍💻 OWNER 🇦🇿** [XAOS KÖLGƏ](https://t.me/sesizKOLGE)",
+"☑ Mən XAOS Federasiyasının Rəsmi Söhbət Botuyam\n\nℹ Botumuz Haqda Qısa Məlumat\n⚡ 𝕏𝔸𝕆𝕊 ℂℍ𝔸𝕋 𝔹𝕆𝕋  Sıradan Söhbət Və Avto Cavablayıcı Botudur. Bu Botun Əsas Özəlliyi Qruplarda Mesaj Sayın Çoxaltmaq , Vaxdınızı Əyləncəli Keçirmək Dir. 𝕏𝔸𝕆𝕊 ℂℍ𝔸𝕋 𝔹𝕆𝕋 u qruplarınlza Alaraq Vaxdınızı Əyləncəli Və Səmərəli Keçirə Bilərsiz.\n\n\n\n➕[❰ QRUPA ƏLAVƏ ET ❱➕](https://t.me/XAOS_Chatbot?startgroup=true)\n\n\n\n**👨‍💻 OWNER 🇦🇿** [XAOS KÖLGƏ](https://t.me/sesizKOLGE)",
 )
