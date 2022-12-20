a5613f1dbd33b3b8ad25cb59a55636b06407183c
Vusaldi <93197397+Vusaldi@users.noreply.github.com>
2022-12-20 12:38:57 +0400

    Update nermin.py

--- nermin.py
+++ nermin.py
@@ -10,7 +10,7 @@
 
 from komekci.aykhan import Nermin
 import base64
-from mesajlar.mesaj import salam, necesen, sagol, getdim, geldim, nermin, ban
+from mesajlar.mesaj import salam, necesen, sagol, getdim, geldim, sesizKOLGE, ban
 from mesajlar.bot import yeni_user, start
 from telethon import events, Button
 import random
